4e5b84508419914a1f020feaa369a7c62f794e69
abramsotocinal <abram.sotocinal@gmail.com>
2019-08-21 16:35:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Frentzy app Backend REST API
+
+## Requirements
+
+JDK 11.0.4
+
+Apache Maven 3.6.0
+
---
+++ src/main/java/com/frentzy/backend/FrentzyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.frentzy.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrentzyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrentzyBackendApplication.class, args);
+	}
+
+}
